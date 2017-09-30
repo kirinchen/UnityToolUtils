ea90e390173d5017f43a8d98ebb0e6bf298de97e
kirinchen <a29243685@msn.com>
2017-09-30 13:58:50 +0800

    it`s done for vote choose

---
+++ Script/Box.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace surfm.tool {
+    public abstract class Box : MonoBehaviour {
+
+        private GAui gaui;
+
+        private bool showed = false;
+
+        public void Awake() {
+            gaui = GetComponent<GAui>();
+        }
+
+
+        private void show(bool b) {
+            if (b != showed) {
+                if (b) {
+                    gaui.MoveIn(GUIAnimSystem.eGUIMove.Self);
+                } else {
+                    gaui.MoveOut(GUIAnimSystem.eGUIMove.Self);
+                }
+                showed = b;
+            }
+        }
+
+
+        public virtual void show(BoxBundle b) {
+            show(true);
+        }
+
+        public void close() {
+            show(false);
+        }
+
+
+    }
+}
---
+++ Script/InputBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace surfm.tool {
+    public class InputBox : Box {
+
+        public InputField inputText;
+        private Bundle current;
+
+
+        public override void show(BoxBundle b) {
+            current = (Bundle)b;
+            base.show(b);
+        }
+
+
+        public void onConfirm() {
+            close();
+            current.doneAction(inputText.text);
+        }
+
+
+        public class Bundle : BoxBundle {
+            public Action<string> doneAction;
+        }
+
+    }
+}
