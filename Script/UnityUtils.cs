1d3fb1f84f2b61542cdec9cb561b4f35fa1a1ecb
kirinchen <a29243685@msn.com>
2017-10-19 15:19:56 +0800

    add screen ceamera shot method

---
+++ Script/UnityUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace surfm.tool {
+    public class UnityUtils : MonoBehaviour {
+        public static void takeCameraShot(Camera camera, int resWidth, int resHeight, string filename) {
+
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            camera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            camera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            camera.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}  exit={1} ", filename, System.IO.File.Exists(filename)));
+
+        }
+
+        public static void takeCameraShot(Camera camera, string filename) {
+            int resWidth = (int)camera.rect.width;
+            int resHeight = (int)camera.rect.height;
+            takeCameraShot(camera, resWidth, resHeight, filename);
+        }
+    }
+}
