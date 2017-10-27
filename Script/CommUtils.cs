8f20b2b84fb440dfa82bf9dd7e9fce6ae3e2b61a
kirinchen <a29243685@msn.com>
2017-10-27 22:24:57 +0800

    create yoar account framework

--- Script/CommUtils.cs
+++ Script/CommUtils.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using UnityEngine;
 namespace surfm.tool {
     public class CommUtils : MonoBehaviour {
+
+        public static string getSha1(string s) {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            return getSha1(bytes);
+        }
 
         public static string getSha1(byte[] bytes) {
             var sha1 = SHA1.Create();
@@ -16,5 +22,40 @@ namespace surfm.tool {
             }
             return sb.ToString();
         }
+
+        public static string encryptBase64(string k,string byteKey, string source) {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] key = Encoding.ASCII.GetBytes(k);
+            byte[] iv = Encoding.ASCII.GetBytes(byteKey);
+            byte[] dataByteArray = Encoding.UTF8.GetBytes(source);
+            des.Key = key;
+            des.IV = iv;
+            string encrypt = "";
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write)) {
+                cs.Write(dataByteArray, 0, dataByteArray.Length);
+                cs.FlushFinalBlock();
+                encrypt = Convert.ToBase64String(ms.ToArray());
+            }
+            return encrypt;
+        }
+
+        public static string decryptBase64(string k, string byteKey, string encrypt) {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] key = Encoding.ASCII.GetBytes(k);
+            byte[] iv = Encoding.ASCII.GetBytes(byteKey);
+            des.Key = key;
+            des.IV = iv;
+
+            byte[] dataByteArray = Convert.FromBase64String(encrypt);
+            using (MemoryStream ms = new MemoryStream()) {
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write)) {
+                    cs.Write(dataByteArray, 0, dataByteArray.Length);
+                    cs.FlushFinalBlock();
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+
     }
 }
