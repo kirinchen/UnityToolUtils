faa14a873716e08e2110422cbbe1affb7394664b
kirinchen <a29243685@msn.com>
2017-11-12 18:25:54 +0800

    it`s fix some android build issue

--- Script/Dialog/DialogManager.cs
+++ Script/Dialog/DialogManager.cs
@@ -5,31 +5,25 @@ using UnityEngine;
 namespace surfm.tool {
     public class DialogManager : MonoBehaviour {
 
-        private static DialogManager instance;
+
 
         private Dictionary<Type, Dialog> map = new Dictionary<Type, Dialog>();
 
         void Awake() {
+
             foreach (Dialog d in GetComponentsInChildren<Dialog>()) {
                 map.Add(d.GetType(), d);
             }
         }
 
 
-        private T _get<T>() where T : Dialog {
+        public T get<T>() where T : Dialog {
             return (T)map[typeof(T)];
         }
 
-        public static T get<T>() where T : Dialog {
-            return getInstance()._get<T>();
-        }
 
-        public static DialogManager getInstance() {
-            if (instance == null) {
-                instance = Instantiate(Resources.Load<DialogManager>("@DialogManager"));
-            }
-            return instance;
-        }
+
+
 
     }
 }
