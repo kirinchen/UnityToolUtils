b354eb4302c8bb76099ee0ae453024ac3bf0d90f
kirinchen <a29243685@msn.com>
2017-11-15 13:48:12 +0800

    hide close tab ui in lobby page

---
+++ Script/Ui/InputFieldBG.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace surfm.tool {
+    public class InputFieldBG : InputField {
+
+
+        public new string text
+        {
+            get
+            {
+                return base.text;
+            }
+            set
+            {
+                base.text = value;
+                if (!isFocused) {
+                    reflesh();
+                }
+            }
+        }
+
+        new void Start() {
+            base.Start();
+            reflesh();
+            onEndEdit.AddListener(onEnd);
+
+        }
+
+        private void reflesh() {
+            targetGraphic.enabled = string.IsNullOrEmpty(text);
+        }
+
+        public override void OnSelect(BaseEventData eventData) {
+            base.OnSelect(eventData);
+            targetGraphic.enabled = true;
+        }
+
+
+        private void onEnd(string s) {
+            reflesh();
+        }
+
+
+
+    }
+}
