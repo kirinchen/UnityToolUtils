a9bfa3f628c16e457abbb986ae6ed908eb2f9709
kirinchen <a29243685@msn.com>
2017-12-19 18:43:52 +0800

    refactory

---
+++ Script/Constant/CommConst.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Reflection;
+using UnityEngine;
+namespace surfm.tool {
+    public class CommConst : MonoBehaviour {
+
+        public void injectCommConst(Action<string, object> cb) {
+            Type t = GetType();
+            FieldInfo[] fis = t.GetFields();
+            foreach (FieldInfo fi in fis) {
+                ConstAttribute ca = (ConstAttribute)Attribute.GetCustomAttribute(fi, typeof(ConstAttribute));
+                if (ca != null) {
+                    object o = fi.GetValue(this);
+                    cb(ca.key, o);
+                }
+            }
+        }
+
+    }
+}
---
+++ Script/Constant/ConstAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace surfm.tool {
+    [AttributeUsage(AttributeTargets.Field, Inherited = false)]
+    public class ConstAttribute : Attribute {
+        public string key;
+
+        public ConstAttribute(string k) {
+            key = k;
+        }
+    }
+}
