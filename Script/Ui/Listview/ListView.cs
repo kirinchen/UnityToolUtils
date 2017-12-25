fa29d79bc197e412663555cdce29e0bfa4259051
kirinchen <a29243685@msn.com>
2017-12-25 19:33:56 +0800

    upgrade new bs lib

--- Script/Ui/Listview/ListView.cs
+++ Script/Ui/Listview/ListView.cs
@@ -1,11 +1,12 @@
 ﻿# if ListViewAdapter
 using frame8.Logic.Misc.Visual.UI.ScrollRectItemsAdapter;
+#endif
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace surfm.tool {
     public class ListView : MonoBehaviour {
-
+#if ListViewAdapter
         public BaseParams parms;
         public List<ItemView.D> prefabList;
         public BaseAdapter adapter { get; private set; }
@@ -15,7 +16,7 @@ namespace surfm.tool {
             adapter.init(prefabList, parms);
             adapter.notifyDataChanage();
         }
+#endif
 
     }
 }
-#endif
