ec4a022d6c0e7d723d5147dbba3a86d76eb0b075
kirinchen <a29243685@msn.com>
2018-01-28 23:53:25 +0800

    it`s can open new room

---
+++ Script/Dialog/InputDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace surfm.tool {
+    public class InputDialog : Dialog {
+        public InputField inputField;
+        public Text titleText;
+        public Button okButton;
+        private Action<string> doneAction;
+
+        new void Awake() {
+            base.Awake();
+            okButton.onClick.AddListener(onOK);
+        }
+
+        private void onOK() {
+            doneAction(inputField.text);
+            show(false);
+        }
+
+        public void show(string t, Action<string> callback) {
+            doneAction = callback;
+            titleText.text = t;
+            show(true);
+        }
+
+
+
+    }
+}
