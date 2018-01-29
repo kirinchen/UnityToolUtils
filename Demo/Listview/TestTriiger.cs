43977e642282ac591c5a5b1301f82a4d916e5f45
kirinchen <a29243685@msn.com>
2018-01-29 17:04:46 +0800

    update List view

--- Demo/Listview/TestTriiger.cs
+++ Demo/Listview/TestTriiger.cs
@@ -1,16 +1,18 @@
-﻿# if ListViewAdapter
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace surfm.tool.demo {
     public class TestTriiger : MonoBehaviour {
 
+# if ListViewAdapter
         public ListView listview;
+#endif
 
         void Start() {
+#if ListViewAdapter
             listview.setAdapter(new DemoAdapter());
+#endif
         }
     }
 }
-#endif
