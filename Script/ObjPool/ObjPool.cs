5c67cf79e3dda87eb10c2529ac735e4e4e4e79bc
kirinchen <a29243685@msn.com>
2018-04-28 17:17:47 +0800

    add pool

---
+++ Script/ObjPool/ObjPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace surfm.tool {
+    public class ObjPool : MonoBehaviour {
+
+        private static ObjPool instance;
+
+        private List<ReuseObj> pool = new List<ReuseObj>();
+
+        public ReuseObj instantiate(ReuseObj original) {
+            ReuseObj ro= findClose(original.GetType());
+            if (ro == null) {
+                original = Instantiate(original);
+                pool.Add(original);
+                original.initCreated();
+                return original;
+            } else {
+                ro.reActive();
+                return ro;
+            }
+        }
+
+        public void close(ReuseObj _o) {
+            bool b= pool.Exists(o => {
+                return _o == o;
+            });
+            if (!b) throw new NullReferenceException("not find this obj in pool");
+            _o.close();
+        }
+
+        private ReuseObj findClose(Type type) {
+             return pool.Find( o=> {
+                return o.isClosed() && o.GetType().Equals(type) ;
+            } );
+        }
+
+        public static ObjPool getInstance() {
+
+
+            if (instance == null) {
+                GameObject go = new GameObject("@ObjPool");
+                instance= go.AddComponent<ObjPool>();
+            }
+            return instance;
+        }
+
+
+    }
+}
---
+++ Script/ObjPool/ReuseObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace surfm.tool {
+    public class ReuseObj : MonoBehaviour {
+
+        public bool closed { get; private set; }
+
+
+        public virtual bool isClosed() {
+            return closed;
+        }
+
+        public virtual void initCreated() {
+            closed = false;
+        }
+
+        public virtual void reActive() {
+            closed = false;
+            gameObject.SetActive(true);
+        }
+
+        public virtual void close() {
+            closed = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
