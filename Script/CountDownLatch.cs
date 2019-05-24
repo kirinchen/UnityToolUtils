d2082fcc69e739fbba2481ea576b660c340375e9
kirin <a29243004@gmail.com>
2019-05-24 23:43:19 +0800

    add CountDownLatch.cs

---
+++ Script/CountDownLatch.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using UnityEngine;
+
+namespace surfm.tool {
+
+    public class CountDownLatch {
+        private object lockObj = new Object();
+        private int counter;
+
+        public CountDownLatch(int counter) {
+            this.counter = counter;
+        }
+
+        public void Await() {
+            lock (lockObj) {
+                while (counter > 0) {
+                    Monitor.Wait(lockObj);
+                }
+            }
+        }
+
+        public void CountDown() {
+            lock (lockObj) {
+                counter--;
+                Monitor.PulseAll(lockObj);
+            }
+        }
+    }
+
+}
