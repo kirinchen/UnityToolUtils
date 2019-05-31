d9ec61e8064206a9c6f233fccab4933537f579f5
kirin <kirin.chen1001@gmail.com>
2019-05-31 15:22:38 +0800

    add Cloner.cs

---
+++ Script/Clone/CloneUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+namespace surfm.tool {
+    public class CloneUtils  {
+
+        public static T clone<T>(object src, T tar) {
+            Cloner cloner = new Cloner(src,tar);
+            return (T)cloner.clone().getTarget();
+        }
+
+    }
+}
---
+++ Script/Clone/Cloner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace surfm.tool {
+
+
+    public class Cloner {
+        private object src;
+        private object tar;
+
+        internal Cloner(object s, object t) {
+            src = s;
+            tar = t;
+        }
+
+        internal Cloner clone() {
+            clone(src,tar);
+            return this;
+        }
+
+
+        private void clone(object s, object t) {
+            List<FieldInfo> sFields = s.GetType().GetFields().ToList();
+            List<FieldInfo> tFields = t.GetType().GetFields().ToList();
+            foreach (FieldInfo sF in sFields) {
+                FieldInfo tField = tFields.Find(tf => { return tf.Name.Equals(sF.Name); });
+                if (sF.ReflectedType.Equals(tField.ReflectedType)) {
+                    object sv = sF.GetValue(s);
+                    tField.SetValue(t, sv);
+                } else {
+                    object sv = sF.GetValue(s);
+                    object tv = tField.GetValue(t);
+                    clone(sv,tv);
+                }
+            }
+        }
+
+        public object getTarget() {
+            return tar;
+        }
+
+
+    }
+
+}
