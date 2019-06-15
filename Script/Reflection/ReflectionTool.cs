815df112cf4d5d8735e836605f8ae9d0854de5fd
kirin <kirin.chen1001@gmail.com>
2019-06-16 00:38:17 +0800

    fix some issue

---
+++ Script/Reflection/ReflectionTool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace surfm.tool {
+
+    public class ReflectionTool  {
+
+        public static List<FieldInfo> listFields<T>() {
+            Type t= typeof(T);
+            return t.GetFields().ToList();
+        }
+
+        public static void setVal( FieldInfo fi, object o , object v) {
+            fi.SetValue(o,v);
+        }
+
+        public static object getVal(FieldInfo fi, object o) {
+            return fi.GetValue(o);
+        }
+
+        public static RexObj<O> newObj<O>(O o) {
+            return new RexObj<O>(o);
+        }
+
+
+
+    }
+}
---
+++ Script/Reflection/RexObj.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+namespace surfm.tool {
+    public class RexObj<T> {
+
+        private List<AsField<T>> fields ;
+        public T obj { get; private set; }
+
+        public RexObj(T t) {
+            obj = t;
+            fields = ReflectionTool.listFields<T>().ConvertAll(f => new AsField<T>(this, f));
+        }
+
+        public List<AsField<T>> listFields() {
+            return fields;
+        }
+
+        public AsField<T> getField(FieldInfo info) {
+            return fields.Find(f=> f.field.Equals(info) );
+        }
+
+        public void setVal(AsField<T> field,object val) {
+            ReflectionTool.setVal(field.field,obj,val);
+        }
+
+        public object getVal(AsField<T> field) {
+            return ReflectionTool.getVal(field.field,obj);
+        }
+
+
+
+
+    }
+
+    public class AsField<FT> {
+
+        public RexObj<FT> obj { get; private set; }
+        public FieldInfo field { get; private set; }
+        internal AsField(RexObj<FT> o, FieldInfo fi) {
+            obj = o;
+            field = fi;
+        }
+
+        public void setVal(object o) {
+            obj.setVal(this, o);
+        }
+
+        public object getVal() {
+            return obj.getVal(this);
+        }
+
+
+    }
+}
