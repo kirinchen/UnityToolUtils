16fd059748e76b7d57d203685f414123c046f86c
kirin <kirin.chen1001@gmail.com>
2019-06-19 20:24:39 +0800

    Add EnumHelper.cs

---
+++ Script/EnumTool/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace surfm.tool {
+
+    public class EnumHelper {
+        public static List<T> ToList<T>() {
+            return Enum.GetValues(typeof(T)).Cast<T>().ToList<T>();
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>() {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
