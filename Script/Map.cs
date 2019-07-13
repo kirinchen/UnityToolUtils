1ad8a72c114a70b615e4fc06ea156e3a0eaa975b
kirin <kirin.chen1001@gmail.com>
2019-07-13 22:02:38 +0800

    pre do calc familys

---
+++ Script/Map.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace surfm.tool {
+
+    public class Map<K,V> : Dictionary<K, V> {
+
+        public Map() { }
+
+        public void put(K fid, V rate) {
+            if (!this.ContainsKey(fid)) {
+                this.Add(fid, rate);
+                return;
+            }
+            this[fid] = rate;
+        }
+
+        public V get(K fid, V defaultV) {
+            if (!this.ContainsKey(fid)) {
+                this.Add(fid, defaultV);
+            }
+            return this[fid];
+        }
+
+    }
+}
