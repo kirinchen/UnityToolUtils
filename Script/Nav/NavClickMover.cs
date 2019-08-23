8da0ebfb333e7c9824fef15aac1bb36c5fde6037
kirin <kirin.chen1001@gmail.com>
2019-08-23 10:19:43 +0800

    done the moveSpeed

--- Script/Nav/NavClickMover.cs
+++ Script/Nav/NavClickMover.cs
@@ -1,14 +1,18 @@
-﻿using UniRx;
+﻿using System;
+using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace surfm.tool {
+    [RequireComponent(typeof(NavMeshAgent))]
     public class NavClickMover : MonoBehaviour {
 
         public LayerMask mask;
         private NavMeshAgent agent;
         private IReactiveProperty<Vector3> targetPos = new ReactiveProperty<Vector3>(Vector3.zero);
         private IReactiveProperty<float> moveSpeed = new ReactiveProperty<float>(30f);
+        public Func<bool> onBeforeMove = () => false;
+
 
         void Awake() {
             agent = GetComponent<NavMeshAgent>();
@@ -30,6 +34,7 @@ namespace surfm.tool {
 
         private void onNewMoveTo() {
             if (!Input.GetMouseButtonUp(0)) return;
+            if (onBeforeMove()) return;
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, mask)) {
