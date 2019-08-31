d4d3307ac35e73a3e8646fa02b2cb69e72dc4e52
kirin <kirin.chen1001@gmail.com>
2019-08-31 23:08:16 +0800

    add Toast

---
+++ Script/Ui/DialogKit/Toast.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace surfm.tool {
+    public class Toast : DialogKit.Dialog {
+
+        public Text text { get; private set; }
+
+        protected override void Awake() {
+            base.Awake();
+            text = GetComponentInChildren<Text>();
+        }
+
+        public Toast toast(string msg,float time) {
+            text.text = msg;
+            show(true);
+            this.delay(()=> show(false),time );
+            return this;
+        }
+
+
+
+    }
+}
