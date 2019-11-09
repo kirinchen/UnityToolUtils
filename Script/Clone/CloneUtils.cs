7e60b62c527e1d224b5ecdab79ada0c9c3124294
kirin <kirin.chen1001@gmail.com>
2019-11-09 15:15:17 +0800

    add Clone/CloneUtils.cs

--- Script/Clone/CloneUtils.cs
+++ Script/Clone/CloneUtils.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using UnityEditor;
-
+using System;
 
 namespace surfm.tool {
     public class CloneUtils  {
 
         public static T cloneByJson<T>(object src) {
             return (T)CommUtils.convert(src,typeof(T));
+        }
 
+        public static object cloneByJson(object src,Type t) {
+            return CommUtils.convert(src, t);
         }
 
         public static T clone<T>(object src, T tar) {
